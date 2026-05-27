4332630dc5a55e06a5eacc296b2a8a228fe4ae9f
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-05-27 09:19:12 +0530

    Add theme provider, UI components & layout updates
    Add theme and UI building blocks and integrate them into the app. New files: ModeToggle, TheamProvider, ui/button, lib/utils and components.json (shadcn config). Update globals.css, layout.tsx and page.tsx to wire up theme provider and UI changes. Also update package.json (add UI/theme-related dependencies) and regenerate package-lock.json.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import { SignInButton, SignUpButton, UserButton, Show } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ModeToggle";
 
 export default function Home() {
   return (
@@ -7,14 +8,17 @@ export default function Home() {
             <Show when="signed-out">
               <SignInButton mode="modal" />
               <SignUpButton>
-                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <Button>
                   Sign Up
-                </button>
+                </Button>
               </SignUpButton>
             </Show>
             <Show when="signed-in">
               <UserButton />
             </Show>
+
+            <ModeToggle />
+
           </header>
   );
 }
